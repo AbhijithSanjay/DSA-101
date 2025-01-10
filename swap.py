eea118023ab6a4e270d7b265d299be2008e00b87
Abhijith S Ramankulath <149100428+AbhijithSanjay@users.noreply.github.com>
2025-01-10 21:49:16 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,6 @@
+x=int(input("Enter first number  -x-: ")) #getting value of x 
+y=int(input("Enter second number -y-: ")) #getting value of y
+z=x #swapping
+x=y
+y=z
+print("After swapping \nx : ",x,"\ny : ",y) #printing the values after swapping
