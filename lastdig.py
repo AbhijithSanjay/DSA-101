480daa10a9196c20d412c42327e9dea2dae3afa1
Abhijith S Ramankulath <149100428+AbhijithSanjay@users.noreply.github.com>
2025-01-13 20:30:00 +0530

    Add files via upload

---
+++ lastdig.py
@@ -0,0 +1,15 @@
+"""Create a program that prints the last digit of a given integer
+
+Create a program that:
+
+Reads the integer
+Prints the last digit
+Warning! Do not use the programming language MAGIC. After you complete the exercise feel free to do 
+so.
+
+Warning! Don't try to convert the number into string etc.
+
+Warning! For this problem it's ok after spending some time to look for the solution."""
+
+n=int(input("enter an integer : "))
+print("last digit : ",n%10)
