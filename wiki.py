0a6afc012d1d50322a1b1404b00847529b6a8655
Abhijith S Ramankulath <149100428+AbhijithSanjay@users.noreply.github.com>
2025-01-14 21:05:33 +0530

    Add files via upload

---
+++ wiki.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the data consumed by wikipedia : "))
+y=int(input("Enter the data consumed by memes : "))
+t=(x*0.1)+(y*0.05)
+if t>100 :
+    print("Too much consumption")
+if y*0.5 > x*0.1:
+    print("WOW MANY MEMES\nSUCH LOL")
