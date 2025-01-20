8b0fdb0624db627cdfa2a828d3a424df1370a736
Abhijith S Ramankulath <149100428+AbhijithSanjay@users.noreply.github.com>
2025-01-20 21:26:36 +0530

    Add files via upload

---
+++ binarySearch.py
@@ -0,0 +1,30 @@
+"""def binarySearch()
+    while low<=high :
+    if l[mid]==tar :
+        print("element found at position : ",mid+1)
+    elif l[mid]>tar :
+        high=mid-1
+    else :
+        low=mid+1"""
+
+n=int(input("Enter the no of elements : "))
+print("enter ",n,"numbers : ")
+l=[]
+for i in range (n):
+    x=int(input())
+    l.append(x)
+tar=int(input("enter the target : "))
+low=0
+high=len(l)
+f=0
+while low<=high and f==0:
+    mid=(low+high)//2
+    if l[mid]==tar :
+        print("element found at position : ",mid+1)
+        f=1
+    elif l[mid]>tar :
+        high=mid-1
+    else :
+        low=mid+1
+
+
